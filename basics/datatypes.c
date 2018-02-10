df7f60eca46e055a3426aaf3c046fd20bcf9e754
Jaya Vignesh J <jayavignesh324@gmail.com>
2018-02-10 17:41:09 +0530

    Data types: int float char double

---
+++ basics/datatypes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* Data Types in C - TN 11th Std Chapter 2 */
+int main()
+{
+    int age = 17;
+    float marks = 95.5;
+    char grade = 'A';
+    double pi = 3.14159;
+
+    printf("Age: %d\n", age);
+    printf("Marks: %.1f\n", marks);
+    printf("Grade: %c\n", grade);
+    printf("Pi: %lf\n", pi);
+    return 0;
+}
